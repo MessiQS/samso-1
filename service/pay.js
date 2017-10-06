71c2e1cd703916e62de99fbb401f901724c5bb6d
mike <573940421@qq.com>
2017-10-06 20:57:33 +0800

    pay

--- service/pay.js
+++ service/pay.js
@@ -14,10 +14,32 @@ module.exports = {
 }
 
 async function wechatPay(ctx, next) {
-	const user_id = "SS0000001",
-		user_ip = "192.168.0.102",
-		attach = "2013-江苏分卷",
-		total_fee = 1;
+	const user_id = ctx.request.body.account || '', //账号，一般为电话号码
+		  attach = ctx.request.body.attach || '',//购买的详细信息
+		  total_fee = ctx.request.body.total_fee,//价格
+		  user_ip = ctx.request.body.user_ip || ''; //ip地址
+	let isValid = true;
+		invalidData;//输入是否合法
+	if(!user_id){
+		isValid = false;
+		invalidData = "账号不能为空";
+	}else if(!attach){
+		isValid = false;
+		invalidData = "购买信息不能为空，请重试";
+	}else if(!total_fee){
+		isValid = false;
+		invalidData = "价格不能为空";
+	}else if(!user_ip){
+		isValid = false;
+		invalidData = "设备地址不能为空";
+	};
+	if(!isValid){
+		ctx.request.body = {
+			'type': false,
+			'data': invalidData
+		}
+		return;
+	}
 	const params = getWechatParams({
 		user_id,
 		user_ip,
