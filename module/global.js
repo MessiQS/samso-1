ac2e6fabafa27e0e306aa8125d16a0a6405ebfd8
mike <573940421@qq.com>
2017-10-23 21:49:32 +0800

    paper download

--- module/global.js
+++ module/global.js
@@ -15,7 +15,7 @@ let provinceObj = {
 async function getProvince(){
 	const nameArr = await getSql('select distinct title from question_banks'),
 	sskey = 'SP00000',
-	idToValue = new Object(),//正向 id -> value
+	// idToValue = new Object(),//正向 id -> value
 	valueToId = new Object();//反向 value -> id
 
 	nameArr.forEach((res, index) => {
@@ -23,11 +23,11 @@ async function getProvince(){
 		let len = index.toString().length,
 			key = sskey.slice(0, 7 - len) + index.toString();
 		valueToId[res.title] = key;
-		idToValue[key] = res.title;
+		// idToValue[key] = res.title;
+		provinceObj[key] = res.title;
 	});
-	provinceObj = idToValue;
 	return {
-		provinceObj:idToValue,
+		provinceObj:provinceObj,
 		getCitysKey:valueToId,
 		paperNameArray:nameArr //所有试卷的集合
 	}
