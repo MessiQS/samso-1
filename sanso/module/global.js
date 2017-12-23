e63f3e0ea99d660d5ed11733d5ea2e4ec93a6d96
mike <573940421@qq.com>
2017-12-23 15:38:22 +0800

    获取paper信息

--- sanso/module/global.js
+++ sanso/module/global.js
@@ -11,9 +11,6 @@ let code = {
 
 };//存储验证码信息
 
-let provinceObj = {
-
-};
 
 //缓存试卷信息
 let paperObj = null;
@@ -21,26 +18,6 @@ let paperObj = null;
 //缓存用户刷题信息，防止接触数据库太多
 let temporaryQuesInfo = []
 
-async function getProvince() {
-	const nameArr = await getSql('select distinct title from question_banks'),
-		sskey = 'SP00000',
-		// idToValue = new Object(),//正向 id -> value
-		valueToId = new Object();//反向 value -> id
-
-	nameArr.forEach((res, index) => {
-		index = index + 1;
-		let len = index.toString().length,
-			key = sskey.slice(0, 7 - len) + index.toString();
-		valueToId[res.title] = key;
-		// idToValue[key] = res.title;
-		provinceObj[key] = res.title;
-	});
-	return {
-		provinceObj: provinceObj,
-		getCitysKey: valueToId,
-		paperNameArray: nameArr //所有试卷的集合
-	}
-}
 async function provinceCache() {
 	if(!!paperObj){
 		return paperObj
@@ -55,7 +32,6 @@ async function provinceCache() {
 }
 module.exports = {
 	codeObj: code,
-	getProvince: getProvince,//获取城市，试卷名相关信息
 	provinceObj: provinceObj,//id与城市的对应
 	provinceCache: provinceCache,//缓存城市
 	temporaryQuesInfo:temporaryQuesInfo
