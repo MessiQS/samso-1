ec0d550a5f920d9153bee671cef1b636f3f7c07a
mike <573940421@qq.com>
2018-02-01 18:37:07 +0800

    add

--- sanso/module/questionBank.js
+++ sanso/module/questionBank.js
@@ -34,7 +34,7 @@ class QuestionBank {
 			}
 			return;
 		}
-		checkBuy(user_id, paperId)
+		// checkBuy(user_id, paperId)
 		const questionArray = await selectFromSql('question_banks', {
 			'FIND_IN_SET': '("' + provinceObjectCache[paperId].title + '",`title`)',
 			'ORDER BY': ` question_number`
